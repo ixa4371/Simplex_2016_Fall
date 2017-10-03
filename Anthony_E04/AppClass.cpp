7d86d98e4b556e0829687ad2002f36732fde2afd
Israel Anthony <ixa4371@rit.edu>
2017-10-02 23:08:44 -0400

    Anthony_E04 now displays a collection of cubes (in the shape of a Space Invader) that gradually move from their starting position towards the right.

--- Anthony_E04/AppClass.cpp
+++ Anthony_E04/AppClass.cpp
@@ -2,15 +2,23 @@
 void Application::InitVariables(void)
 {
 	////Change this to your name and email
-	//m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
+	//m_sProgrammer = "Israel Anthony - ixa4371@rit.edu";
 
 	////Alberto needed this at this position for software recording.
 	//m_pWindow->setPosition(sf::Vector2i(710, 0));
 	
-	//init the mesh
-	m_pMesh = new MyMesh();
-	m_pMesh->GenerateCube(1.0f, C_WHITE);
-	m_pMesh->GenerateSphere(1.0f, 5, C_WHITE);
+	// Init the 46 meshes in the vector collection to create the image
+	for (int i = 0; i < 46; i++)
+	{
+		MyMesh* mesh = new MyMesh();
+		cubes.push_back(mesh);
+	}
+
+	// Generate the cubes for each of the meshes
+	for (int i = 0; i < 46; i++)
+	{
+		cubes[i]->GenerateCube(1.0f, C_BLACK);
+	}
 }
 void Application::Update(void)
 {
@@ -28,19 +36,86 @@ void Application::Display(void)
 	// Clear the screen
 	ClearScreen();
 
+	// Calculate view and projection matricies
 	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
 	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
 	
-	matrix4 m4Scale = glm::scale(IDENTITY_M4, vector3(2.0f,2.0f,2.0f));
+	// Calculate the scale for the cubes (all the same size)
+	matrix4 m4Scale = glm::scale(IDENTITY_M4, vector3(1.0f,1.0f,1.0f));
+
+	// Calculate the translations of the cubes by row
 	static float value = 0.0f;
-	matrix4 m4Translate = glm::translate(IDENTITY_M4, vector3(value, 2.0f, 3.0f));
-	value += 0.01f;
-
-	//matrix4 m4Model = m4Translate * m4Scale;
-	matrix4 m4Model = m4Scale * m4Translate;
-
-	m_pMesh->Render(m4Projection, m4View, m4Model);
+	std::vector<matrix4> translations;
 	
+	for (int i = -5; i <= 5; i++) // row1
+	{
+		if (i == -3 || i == 3)
+		{
+			translations.push_back(glm::translate(IDENTITY_M4, vector3(i + value, 4.0f, -20.0f)));
+		}
+	}
+
+	for (int i = -5; i <= 5; i++) // row2
+	{
+		if (i == -2 || i == 2)
+		{
+			translations.push_back(glm::translate(IDENTITY_M4, vector3(i + value, 3.0f, -20.0f)));
+		}
+	}
+
+	for (int i = -5; i <= 5; i++) // row3
+	{
+		if (i > -4 && i < 4)
+		{
+			translations.push_back(glm::translate(IDENTITY_M4, vector3(i + value, 2.0f, -20.0f)));
+		}
+	}
+
+	for (int i = -5; i <= 5; i++) // row4
+	{
+		if (i != -5 && i != -2 && i != 2 && i != 5)
+		{
+			translations.push_back(glm::translate(IDENTITY_M4, vector3(i + value, 1.0f, -20.0f)));
+		}
+	}
+
+	for (int i = -5; i <= 5; i++) // row5
+	{
+		translations.push_back(glm::translate(IDENTITY_M4, vector3(i + value, 0.0f, -20.0f)));
+	}
+
+	for (int i = -5; i <= 5; i++) // row6
+	{
+		if (i != -4 && i != 4)
+		{
+			translations.push_back(glm::translate(IDENTITY_M4, vector3(i + value, -1.0f, -20.0f)));
+		}
+	}
+
+	for (int i = -5; i <= 5; i++) // row7
+	{
+		if (i == -5 || i == -3 || i == 3 || i == 5)
+		{
+			translations.push_back(glm::translate(IDENTITY_M4, vector3(i + value, -2.0f, -20.0f)));
+		}
+	}
+
+	for (int i = -5; i <= 5; i++) // row8
+	{
+		if (i == -2 || i == -1 || i == 1 || i == 2)
+		{
+			translations.push_back(glm::translate(IDENTITY_M4, vector3(i + value, -3.0f, -20.0f)));
+		}
+	}
+
+	value += 0.01f; // move cubes across screen
+
+	// Render Cubes
+	for (int i = 0; i < 46; i++)
+	{
+		cubes[i]->Render(m4Projection, m4View, (m4Scale * translations[i]));
+	}
+
 	// draw a skybox
 	m_pMeshMngr->AddSkyboxToRenderList();
 	
@@ -58,7 +133,11 @@ void Application::Display(void)
 }
 void Application::Release(void)
 {
-	SafeDelete(m_pMesh);
+	// Delete Cubes
+	for (int i = 0; i < 46; i++)
+	{
+		SafeDelete(cubes[i]);
+	}
 
 	//release GUI
 	ShutdownGUI();
--- Anthony_E04/AppClass.h
+++ Anthony_E04/AppClass.h
@@ -14,8 +14,9 @@ Date: 2017/05
 
 class Application
 {
-	MyMesh* m_pMesh = nullptr;
-	String m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
+	std::vector<MyMesh*> cubes; // Cubes used in the display
+
+	String m_sProgrammer = "Israel Anthony - ixa4371@rit.edu";
 
 private:
 	static ImGuiObject gui; //GUI object
